b5abb8000c641139e25dece84a90ebe55b763560
Hüseyin Karadana <huseyinkaradana35@gmail.com>
2024-04-09 01:01:03 +0300

    Add project files.

---
+++ ChatApps/Controllers/testController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApps.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
